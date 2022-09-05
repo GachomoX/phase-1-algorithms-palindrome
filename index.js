3a4b11f5bd202a0a74f499462806b1e113d34749
Dennis <dennisgachomo@gmail.com>
2022-09-05 16:15:40 +0300

    complete

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+
+  let wordArray = word.split('');
+  let reversed = wordArray.reverse().join('');
+
+    if(word === reversed){
+      return true;
+    } else {
+    return false;
+    }
+  }
+
+
 
 /* 
   Add your pseudocode here
+  let wordArray be string solit into array
+  reverse array
+  if(word at index is the same as wordReversed at index)
+  return true
+  else 
+  return false 
 */
 
 /*
@@ -15,11 +32,8 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
